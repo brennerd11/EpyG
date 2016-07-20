c2b7306864ac751d9bba708ad1215b4f5ed1515b
brennerd <daniel.brenner@dzne.de>
2016-07-20 16:42:37 +0200

    Operations can now be multiplied with other operators to yield a composite operator
    Most operators and the epg object now have html and json representation for (a) rich rendering and (b) a first step towards serialisation

--- EpyG/EpyG.py
+++ EpyG/EpyG.py
@@ -9,6 +9,8 @@ Derived from Ed Prachts python implementation
 """
 from __future__ import division, print_function
 
+from collections import OrderedDict
+
 import numpy as np
 import sys
 from numpy import sin as sin
@@ -33,8 +35,10 @@ class EpyG(object):
 
     '''
 
-    # Alternatively allow here to construct the EPG from another EPG 
-
+    # NOTE: This is a plain straight forward implementation where the index in the state array corresponds to the state
+    # NOTE: It would be much(!) more efficient for certain applications to have a sparse state matrix and/or to abstract
+    #       access to the individual states to isolate this from the actual data structure at hand. This would probably
+    #       conflict with the way operators are currently used!
     def __init__(self, initial_size=256, m0=1.0):
         '''
         Constructs EpyG object
@@ -47,8 +51,9 @@ class EpyG(object):
         '''
         # State vector: index 0,1 transverse dephased states; index 2 longitudinal dephased states 
         self.state      = np.zeros((3 , initial_size), dtype=DTYPE)  # Should encapsulate the state and make it a property
-        self.state[2, 0]= m0 # Use meq as equilibrium magnetisation; otherwise use the specified m0
-        self.max_state = 0 # Maximum order of occupied states
+        self.state[2, 0]= m0  # Use meq as equilibrium magnetisation; otherwise use the specified m0
+        self.max_state = 0  # Maximum order of occupied states
+        self.print_digits = 3  # Used in ipython notebooks to control precision/threshold for vis
 
     @staticmethod
     def copy(other_epg):
@@ -143,7 +148,7 @@ class EpyG(object):
         try:
             return self.state[2, order]
         except IndexError: # Everything that is not populated is 0!
-            return np.arrray(0.0, dtype=DTYPE) # TODO Here it care should be taken to return identical datatypes as when returning a matrix element
+            return np.arrray(0.0, dtype=self.state.dtype)
 
     def get_F(self, order=0, rx_phase=0.0):
         '''
@@ -158,39 +163,172 @@ class EpyG(object):
             return (self.state[idx, abs(order)] * theta)
         except IndexError: # Everything that is not populated is 0!
             return np.arrray(0.0, dtype=DTYPE)
-        
-        
+
+    def is_occupied(self, k, thresh=1e-6):
+        if np.any(np.abs(self.state[:,k]) > thresh):
+            return True
+        else:
+            return False
+
+    def state_iterator(self, thresh=1e-6):
+        '''
+        Iterates over all non-empty states returning the tuple (k, [F+, F-, Z])
+        '''
+
+        for k in range(self.max_state+1):
+            if self.is_occupied(k, thresh):
+                yield (k, self.state[:, k])
+
+    def plot(self, axis=None):
+        import matplotlib.pylab as plt
+
+        if axis is None:
+            _, axis = plt.subplots()
+
+        transverse = np.hstack((self.state[1, self.max_state+1:1:-1], self.state[0, 0:self.max_state+1]))
+        index = np.arange(-self.max_state, self.max_state+1)
+
+        print(index.shape)
+        print(transverse.shape)
+        axis.plot(index, np.abs(transverse), "o")
+
+    def _repr_html_(self):
+        """
+        Tabular representation of the EPG
+        Very limited. Maybe color coding the cells would be nice
+
+        """
+        cell_spec = "<td>{0:." + str(self.print_digits) + "f} </td>"
+        thresh = np.power(10, -self.print_digits)
+
+        html = ["<table>"]
+        html.append("<tr>")
+        html.append("<td><b>k</b></td>")
+
+        # k row
+        for state in self.state_iterator(thresh):
+            html.append("<td><b>{0}</b></td>".format(state[0]))
+        html.append("</tr>")
+        html.append("<tr>")
+
+        # F+ row
+        html.append("<td><b>F+</b></td>")
+
+        for state in self.state_iterator(thresh):
+            html.append(cell_spec.format(state[1][0]))
+
+        html.append("</tr>")
+        html.append("<tr>")
+
+        # F- row
+        html.append("<td><b>F-</b></td>")
+
+        for state in self.state_iterator(thresh):
+            html.append(cell_spec.format(state[1][1]))
+
+        html.append("</tr>")
+        html.append("<tr>")
+
+        # Z row
+        html.append("<td><b>Z</b></td>")
+
+        for state in self.state_iterator():
+            html.append(cell_spec.format(state[1][2]))
+
+        html.append("</tr>")
+        html.append("</table>")
+
+        return ''.join(html)
+
+    def _repr_json_(self):
+        jsonrepr = OrderedDict()
+        jsonrepr['type'] = self.__class__.__name__
+        jsonrepr['size'] = self.size()
+        jsonrepr['k'] = range(self.max_state)
+        jsonrepr['F+'] = OrderedDict()
+        jsonrepr['F-'] = OrderedDict()
+        jsonrepr['Z'] = OrderedDict()
+
+        jsonrepr['F+']['real'] = self.state[0, :self.max_state].real.tolist()
+        jsonrepr['F-']['real'] = self.state[1, :self.max_state].real.tolist()
+        jsonrepr['Z']['real'] = self.state[2, :self.max_state].real.tolist()
+
+        jsonrepr['F+']['imag'] = self.state[0, :self.max_state].imag.tolist()
+        jsonrepr['F-']['imag'] = self.state[1, :self.max_state].imag.tolist()
+        jsonrepr['Z']['imag'] = self.state[2, :self.max_state].imag.tolist()
+
+        return jsonrepr
+
+    # def __str__(self):
+    #     spec = "{0:6." + str(self.print_digits) + "f}"
+    #     thresh = np.power(10, -self.print_digits)
+    #
+    #     outstr = [""]
+    #
+    #     # k row
+    #     for state in self.state_iterator(thresh):
+    #         outstr.append("{0:5d}".format(state[0]))
+    #     outstr.append("\n")
+    #
+    #     # F+ row
+    #     for state in self.state_iterator(thresh):
+    #         outstr.append(spec.format(state[1][0]))
+    #     outstr.append("\n")
+    #
+    #     # F- row
+    #
+    #     for state in self.state_iterator(thresh):
+    #         outstr.append(spec.format(state[1][1]))
+    #         outstr.append("\n")
+    #     # Z row
+    #
+    #     for state in self.state_iterator():
+    #         outstr.append(spec.format(state[1][2]))
+    #
+    #     return ''.join(outstr)
+
+
 class Operator(object):
     '''
     Base class of an operator acting on an epg object. Application of the operator will alter the EPG object!
     All derived operators should make sure to return the epg on application to allow operator chaining.
 
-    Operators should encapsulate a abstract modification of the spin sates - 
-    e.g. for example for relaxation times only the direct exponentials are supplied.
+    Operators should encapsulate a abstract modification of the spin sates
 
     For physical simulation there shall be another layer that ties the operators together...
     '''
 
     def __init__(self, name=""):
         self.count = 0  # How often has this operator been called
-        self.name = name # Optional name for operators
+        self.name = name  # Optional name for operators
 
     def apply(self, epg):
         self.count += 1  # Count applications of the operator
         return epg
 
-    def __mul__(self, epg):
+    def __mul__(self, other):
         # Overload multiplication just calling the apply method - intentionally rmul is not defined!
         # TODO This has to move to the apply method to have it called always
-        if not hasattr(epg, "state"):
+        if isinstance(other, Operator):
+            return CompositeOperator(self, other)
+        elif not hasattr(other, "state"):
             raise NotImplementedError("Can not apply operator to non-EPGs")
-        return self.apply(epg)
+        else:
+            return self.apply(other)
 
     def __call__(self, other):
         return self.apply(other)
 
     def __str__(self):
         return self.__class__.__name__
+
+    def _repr_json_(self):
+        reprjsondict = OrderedDict()
+        reprjsondict["type"] = self.__class__.__name__
+        reprjsondict["name"] = self.name
+        reprjsondict["count"] = self.count
+
+        return reprjsondict
 
 
 class Transform(Operator):
@@ -237,7 +375,7 @@ class Transform(Operator):
         self._changed = True
 
     def calc_matrix(self):
-        # TODO CHECK WITH THE ONE FROM Hargreaves... is that correct???? <<< Seems so...
+        # CHECK WITH THE ONE FROM Hargreaves... is that correct???? <<< Seems so...
         # Recaluclates the mixing matrix using the alpha and phi members
 
         #alpha = -self._alpha #This is required for correct rotation!
@@ -245,9 +383,9 @@ class Transform(Operator):
         phi = self._phi
         co = cos(alpha)
         si = sin(alpha)
-        ph = exp(self._phi * 1j)
+        ph = exp(phi * 1j)
         ph_i = 1.0 / ph
-        ph2 = exp(2.0 * self._phi * 1j)
+        ph2 = exp(2.0 * phi * 1j)
         ph2_i = 1.0 / ph2
 
         #self._R[0, 0] = cos(alpha/2.0)**2
@@ -283,6 +421,12 @@ class Transform(Operator):
         epg.state = np.dot(self._R, epg.state)
         return super(Transform, self).apply(epg)  # Invoke superclass method
 
+    def _repr_json_(self):
+        reprjsondict =  super(Transform, self)._repr_json_()
+        reprjsondict["alpha"] = self._alpha
+        reprjsondict["phi"] = self._phi
+
+        return reprjsondict
 
 class PhaseIncrementedTransform(Transform):
     '''
@@ -326,6 +470,12 @@ class PhaseIncrementedTransform(Transform):
         self.update()
         return super(PhaseIncrementedTransform, self).apply(epg)
 
+    def _repr_json_(self):
+        reprjsondict =  super(PhaseIncrementedTransform, self)._repr_json_()
+        reprjsondict["linear_phase_increment"] = self.linear_phase_increment
+        reprjsondict["constant_phase_increment"] = self.constant_phase_increment
+        return reprjsondict
+
 
 class Epsilon(Operator):
     '''
@@ -344,7 +494,14 @@ class Epsilon(Operator):
         epg.state[2  ,0] += self._meq*(1.0-self._E1)   # Regrowth of Mz
         
         return super(Epsilon, self).apply(epg)
-        
+
+    def _repr_json_(self):
+        reprjsondict =  super(Epsilon, self)._repr_json_()
+        reprjsondict["E1"] = self._E1
+        reprjsondict["E2"] = self._E2
+        reprjsondict["meq"] = self._meq
+        return reprjsondict
+
         
 class Shift(Operator):
     '''
@@ -366,13 +523,13 @@ class Shift(Operator):
         self._autogrow = autogrow
 
     def apply(self, epg):
-        epg.max_state += self.shifts # TODO Increment the state counter -> be careful here... if exception occurs max_state will have wrong value
+        epg.max_state += self.shifts # Increment the state counter -> be careful here... if exception occurs max_state will have wrong value
 
         if self._autogrow and epg.max_state > epg.size():
             epg.extend()
 
         # TODO Make multiple shifts happen without the for loop
-        for i in xrange(np.abs(self.shifts)):
+        for i in range(np.abs(self.shifts)):
             if self.shifts > 0: # Here we handle positive shifts !!! NEED TO DOUBLE CHECK THIS !!!
                 #epg.state[0,self.shifts+1:epg.max_state+self.shifts+1]   = epg.state[0, 0:epg.max_state+1] # Shift first row to the right
                 epg.state[0, 1:] = epg.state[0, 0:-1]
@@ -380,13 +537,21 @@ class Shift(Operator):
                 # Shift this one up (dephased transverse crosses zero)
                 #epg.state[1,0:epg.max_state] = epg.state[1, 1:epg.max_state-1] # Shift this one left
                 epg.state[1,0:-1] = epg.state[1, 1:] # Shift this one left
-                epg.state[0,0 ] = np.conj(epg.state[1, 0   ])
-            else:  # TODO CHECK THIS PART!
-                epg.state[1,1:] = epg.state[1, 0:-1]
+                epg.state[0,0 ] = np.conj(epg.state[1, 0])
+            elif self.shifts < 0:  # TODO CHECK THIS PART!
+                epg.state[1, 1:] = epg.state[1, 0:-1]
                 epg.state[0, 0:-1] = epg.state[0, 1:]
-                epg.state[1,0 ] = np.conj(epg.state[0, 0])
+                epg.state[1, 0] = np.conj(epg.state[0, 0])
+            else:  # Else is 0 shift - do nothing
+                pass
 
         return super(Shift, self).apply(epg)
+
+    def _repr_json_(self):
+        reprjsondict =  super(Shift, self)._repr_json_()
+        reprjsondict["shifts"] = self.shifts
+
+        return reprjsondict
         
 
 class Diffusion(Operator):
@@ -396,12 +561,12 @@ class Diffusion(Operator):
     See e.g. Nehrke et al. MRM 2009 RF Spoiling for AFI paper
     '''
 
-    def __init__(self, d):
+    def __init__(self, d, *args, **kwargs):
         '''
         :param d: dimension less diffusion damping constant - corresponding to b*D were D is diffusivity and b is "the b-value"
         :type d: scalar, floating point
         '''
-        super(Diffusion, self).__init__()
+        super(Diffusion, self).__init__(*args, **kwargs)
         self._d = d
 
     def apply(self, epg):
@@ -424,6 +589,12 @@ class Diffusion(Operator):
 
         return super(Diffusion, self).apply(epg)
 
+    def _repr_json_(self):
+        reprjsondict = super(Diffusion, self)._repr_json_()
+        reprjsondict["d"] = self._d
+
+        return reprjsondict
+
 
 class Spoil(Operator):
     '''
@@ -432,7 +603,7 @@ class Spoil(Operator):
 
     def __init__(self, compact_states=False, *args, **kwargs):
         super(Spoil, self).__init__(*args, **kwargs)
-        self.compact_states=compact_states
+        self.compact_states = compact_states
 
     def apply(self, epg):
         epg.state[0, :] = 0.0
@@ -448,11 +619,11 @@ class Observer(Operator):
     Stores EPG values - does NOT modify the EPG
     '''
 
-    def __init__(self, F_states=(0,), Z_states=(0,), rx_phase=0.0):
-        super(Observer, self).__init__()
+    def __init__(self, F_states=(0,), Z_states=(0,), rx_phase=0.0, *args, **kwargs):
+        super(Observer, self).__init__(*args, **kwargs)
         self._data_dict_f = dict()
         self._data_dict_z = dict()
-        self.rx_phase = rx_phase  # Tramsverse detection phase
+        self.rx_phase = rx_phase  # Transverse detection phase
 
         for f_state in F_states:
             self._data_dict_f[f_state] = []
@@ -501,16 +672,62 @@ class Observer(Operator):
 
         return super(Observer, self).apply(epg)
 
+    def _repr_html_(self):
+        cell_spec = "<td>{0:." + str(3) + "f} </td>"
+
+        html = []
+        html.append("<h3>Transverse</h3>")
+
+        html.append("<table>")
+        for state in self._data_dict_f.keys():
+            html.append("<tr>")
+            html.append("<td><b>{0}</b></td>".format(state))
+            # k row
+            for element in self._data_dict_f[state]:
+                html.append(cell_spec.format(element))
+            html.append("</tr>")
+        html.append("</table>")
+
+        html.append("<h3>Longitudinal</h3>")
+        html.append("<table>")
+        for state in self._data_dict_z.keys():
+            html.append("<tr>")
+            html.append("<td><b>{0}</b></td>".format(state))
+            # k row
+            for element in self._data_dict_z[state]:
+                html.append(cell_spec.format(element))
+            html.append("</tr>")
+        html.append("</table>")
+
+        return "".join(html)
+
+    def _repr_json_(self):
+        reprjsondict = super(Observer, self)._repr_json_()
+        reprjsondict["rx_phase"] = self.rx_phase
+        reprjsondict["F"] = OrderedDict()
+        reprjsondict["Z"] = OrderedDict()
+
+        for state in self._data_dict_f.keys():
+            reprjsondict["F"][state] = {"real": self.get_F(state).real.tolist(),
+                                        "imag": self.get_F(state).imag.tolist()}
+
+        for state in self._data_dict_z.keys():
+            reprjsondict["Z"][state] = {"real": self.get_Z(state).real.tolist(),
+                                        "imag": self.get_Z(state).imag.tolist()}
+
+        return reprjsondict
 
 class CompositeOperator(Operator):
     """
     Composite operator that contains several operators
-
     """
 
-    def __init__(self, operators=()):
-        super(CompositeOperator, self).__init__()
-        self._operators = list(operators)
+    def __init__(self, *args, **kwargs):
+        super(CompositeOperator, self).__init__(**kwargs)
+        self._operators = []
+
+        for op in args:
+            self.append(op)
 
     def prepend(self, operator):
         self._operators.insert(0, operator)
@@ -537,7 +754,7 @@ class CompositeOperator(Operator):
         for op in reversed(self._operators):
             epg_dash = op.apply(epg_dash)
 
-        return epg_dash
+        return super(CompositeOperator, self).apply(epg)
 
     def __mul__(self, other):
         if hasattr(other, "state"):
@@ -553,6 +770,11 @@ class CompositeOperator(Operator):
         else:
             raise NotImplementedError("No viable multiplication for composite operator")
 
+    def _repr_json_(self):
+        reprjsondict = super(CompositeOperator, self)._repr_json_()
+        reprjsondict["operators"] = OrderedDict()
 
+        for i, op in enumerate(self._operators):
+            reprjsondict["operators"][i] = op._repr_json_()
 
-
+        return reprjsondict
